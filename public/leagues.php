808883e9a51d8ab494abd3d42c5dd58cab6e2d85
pasamaar11 <jpasamaracon@gmail.com>
2025-11-11 17:49:22 +0100

    he añadido mas ligas, mas partidos, mas equipos

--- public/leagues.php
+++ public/leagues.php
@@ -5,7 +5,11 @@ include_once('../includes/auth_check.php');
 // Lista de ligas
 $ligas = [
     "Liga 1" => ["categoria" => "Juvenil"], 
-    "Liga 2" => ["categoria" => "Regional"]
+    "Liga 2" => ["categoria" => "Regional"],
+    "Liga 3" => ["categoria" => "Alevín"],
+    "Liga 4" => ["categoria" => "Infantil"],
+    "Liga 5" => ["categoria" => "Juvenil Femenino"],
+    "Liga 6" => ["categoria" => "Regional Femenino"]
 ];
 
 // Liga seleccionada (por defecto Liga 1)
@@ -13,25 +17,84 @@ $ligaSeleccionada = $_POST['liga'] ?? "Liga 1";
 
 // Equipos por liga
 $equiposPorLiga = [
-    "Liga 1" => ["Valdefierro","Delicias","Escalerillas","Oliver","Pina de Ebro","San José","Casablanca","Montañana"],
-    "Liga 2" => ["Casetas","Caspe","Illueca","Utebo","La Almunia","Ejea","Tarazona","Calatayud"]
+    "Liga 1" => ["Valdefierro","Delicias","Oliver","San José","Casablanca","Montañana","Escalerillas","La Jota"],
+    "Liga 2" => ["Casetas","Rosales","Miralbueno","Utebo","La Almozara","Torrero","San Gregorio","San José"],
+    "Liga 3" => ["Actur","Oliver","San José","Casablanca","La Jota","Montecanal","Valdefierro","Delicias"],
+    "Liga 4" => ["Montañana","Escalerillas","Casetas","Miralbueno","Rosales","San Gregorio","La Almozara","Torrero"],
+    "Liga 5" => ["Oliver","Actur","San José","Valdefierro","Casablanca","La Jota","Delicias","Montecanal"],
+    "Liga 6" => ["Torrero","La Almozara","Miralbueno","Rosales","Escalerillas","Montañana","San Gregorio","Oliver"]
 ];
+
 
 // Partidos por liga
 $partidosPorLiga = [
     "Liga 1" => [
         ["local"=>"Valdefierro","visitante"=>"Delicias","resultado"=>"2-1"],
-        ["local"=>"Escalerillas","visitante"=>"Oliver","resultado"=>"0-0"],
-        ["local"=>"Pina de Ebro","visitante"=>"San José","resultado"=>"1-3"],
-        ["local"=>"Casablanca","visitante"=>"Montañana","resultado"=>"4-2"]
+        ["local"=>"Oliver","visitante"=>"San José","resultado"=>"1-1"],
+        ["local"=>"Casablanca","visitante"=>"Montañana","resultado"=>"3-2"],
+        ["local"=>"Escalerillas","visitante"=>"La Jota","resultado"=>"0-0"],
+        ["local"=>"Delicias","visitante"=>"Oliver","resultado"=>"1-2"],
+        ["local"=>"San José","visitante"=>"Valdefierro","resultado"=>"0-3"],
+        ["local"=>"Montañana","visitante"=>"Escalerillas","resultado"=>"2-2"],
+        ["local"=>"La Jota","visitante"=>"Casablanca","resultado"=>"1-1"]
     ],
+
     "Liga 2" => [
-        ["local"=>"Casetas","visitante"=>"Caspe","resultado"=>"1-0"],
-        ["local"=>"Illueca","visitante"=>"Utebo","resultado"=>"0-0"],
-        ["local"=>"La Almunia","visitante"=>"Ejea","resultado"=>"1-3"],
-        ["local"=>"Tarazona","visitante"=>"Calatayud","resultado"=>"4-2"]
+        ["local"=>"Casetas","visitante"=>"Rosales","resultado"=>"2-0"],
+        ["local"=>"Miralbueno","visitante"=>"Utebo","resultado"=>"1-1"],
+        ["local"=>"La Almozara","visitante"=>"Torrero","resultado"=>"0-3"],
+        ["local"=>"San Gregorio","visitante"=>"San José","resultado"=>"1-2"],
+        ["local"=>"Rosales","visitante"=>"Miralbueno","resultado"=>"2-2"],
+        ["local"=>"Utebo","visitante"=>"Casetas","resultado"=>"1-1"],
+        ["local"=>"Torrero","visitante"=>"San Gregorio","resultado"=>"3-0"],
+        ["local"=>"San José","visitante"=>"La Almozara","resultado"=>"2-1"]
+    ],
+
+    "Liga 3" => [
+        ["local"=>"Actur","visitante"=>"Oliver","resultado"=>"0-2"],
+        ["local"=>"San José","visitante"=>"Casablanca","resultado"=>"1-1"],
+        ["local"=>"La Jota","visitante"=>"Montecanal","resultado"=>"3-1"],
+        ["local"=>"Valdefierro","visitante"=>"Delicias","resultado"=>"2-2"],
+        ["local"=>"Oliver","visitante"=>"San José","resultado"=>"1-0"],
+        ["local"=>"Casablanca","visitante"=>"Actur","resultado"=>"2-2"],
+        ["local"=>"Montecanal","visitante"=>"La Jota","resultado"=>"0-1"],
+        ["local"=>"Delicias","visitante"=>"Valdefierro","resultado"=>"1-3"]
+    ],
+
+    "Liga 4" => [
+        ["local"=>"Montañana","visitante"=>"Escalerillas","resultado"=>"2-2"],
+        ["local"=>"Casetas","visitante"=>"Miralbueno","resultado"=>"1-1"],
+        ["local"=>"Rosales","visitante"=>"San Gregorio","resultado"=>"0-3"],
+        ["local"=>"La Almozara","visitante"=>"Torrero","resultado"=>"2-2"],
+        ["local"=>"Escalerillas","visitante"=>"Montañana","resultado"=>"1-2"],
+        ["local"=>"Miralbueno","visitante"=>"Rosales","resultado"=>"2-0"],
+        ["local"=>"San Gregorio","visitante"=>"La Almozara","resultado"=>"1-1"],
+        ["local"=>"Torrero","visitante"=>"Casetas","resultado"=>"3-1"]
+    ],
+
+    "Liga 5" => [
+        ["local"=>"Oliver","visitante"=>"Actur","resultado"=>"1-0"],
+        ["local"=>"San José","visitante"=>"Valdefierro","resultado"=>"0-2"],
+        ["local"=>"Casablanca","visitante"=>"La Jota","resultado"=>"2-1"],
+        ["local"=>"Delicias","visitante"=>"Montecanal","resultado"=>"1-1"],
+        ["local"=>"Actur","visitante"=>"San José","resultado"=>"1-2"],
+        ["local"=>"Valdefierro","visitante"=>"Oliver","resultado"=>"3-1"],
+        ["local"=>"Montecanal","visitante"=>"Casablanca","resultado"=>"0-2"],
+        ["local"=>"La Jota","visitante"=>"Delicias","resultado"=>"1-1"]
+    ],
+
+    "Liga 6" => [
+        ["local"=>"Torrero","visitante"=>"La Almozara","resultado"=>"2-1"],
+        ["local"=>"Miralbueno","visitante"=>"Rosales","resultado"=>"0-0"],
+        ["local"=>"Escalerillas","visitante"=>"Montañana","resultado"=>"1-3"],
+        ["local"=>"San Gregorio","visitante"=>"Oliver","resultado"=>"1-2"],
+        ["local"=>"La Almozara","visitante"=>"Miralbueno","resultado"=>"2-2"],
+        ["local"=>"Rosales","visitante"=>"Torrero","resultado"=>"1-1"],
+        ["local"=>"Montañana","visitante"=>"San Gregorio","resultado"=>"3-0"],
+        ["local"=>"Oliver","visitante"=>"Escalerillas","resultado"=>"2-2"]
     ]
 ];
+
 
 $equipos = $equiposPorLiga[$ligaSeleccionada];
 $partidos = $partidosPorLiga[$ligaSeleccionada];
--- public/teams.php
+++ public/teams.php
@@ -3,12 +3,16 @@ include_once('../includes/header.php');
 include_once('../includes/auth_check.php');
 
 // Lista de ligas
-$ligas = ["Liga 1","Liga 2"];
+$ligas = ["Liga 1","Liga 2","Liga 3","Liga 4","Liga 5","Liga 6"];
 $ligaSeleccionada = $_POST['liga'] ?? "Liga 1";
 
 $equiposPorLiga = [
-    "Liga 1" => ["Valdefierro","Delicias","Escalerillas","Oliver","Pina de Ebro","San José","Casablanca","Montañana"],
-    "Liga 2" => ["Casetas","Caspe","Illueca","Utebo","La Almunia","Ejea","Tarazona","Calatayud"]
+    "Liga 1" => ["Valdefierro","Delicias","Oliver","San José","Casablanca","Montañana","Escalerillas","La Jota"],
+    "Liga 2" => ["Casetas","Rosales","Miralbueno","Utebo","La Almozara","Torrero","San Gregorio","San José"],
+    "Liga 3" => ["Actur","Oliver","San José","Casablanca","La Jota","Montecanal","Valdefierro","Delicias"],
+    "Liga 4" => ["Montañana","Escalerillas","Casetas","Miralbueno","Rosales","San Gregorio","La Almozara","Torrero"],
+    "Liga 5" => ["Oliver","Actur","San José","Valdefierro","Casablanca","La Jota","Delicias","Montecanal"],
+    "Liga 6" => ["Torrero","La Almozara","Miralbueno","Rosales","Escalerillas","Montañana","San Gregorio","Oliver"]
 ];
 
 $equipos = $equiposPorLiga[$ligaSeleccionada];
